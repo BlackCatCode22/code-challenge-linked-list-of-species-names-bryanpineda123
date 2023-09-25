d7854fea3c7a8e59df55ad0a409a74baee0e09d3
bryan <141785744+bryanpineda123@users.noreply.github.com>
2023-09-25 11:56:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,34 +4,64 @@
 
 using namespace std;
 
+// Define a structure to represent an animal node
+struct AnimalNode {
+    string animalName;
+    AnimalNode* next;
+};
+
 int main()
 {
-
     fstream myFile;
-    myFile.open("C:\\Users\\BE129\\Desktop\\animalNames.txt", ios::in);//read REMEMBER TO REDIRECT PATH TO YOUR LOCAL MEMORY
+    myFile.open("C:\\Users\\BE129\\Downloads\\animalNames.txt", ios::in); //Remember to redirect Path in your local memory
 
-    string fileContents;
-
-    if(myFile.is_open())
+    if (myFile.is_open())
     {
         string line;
-        while(getline(myFile, line))
+        AnimalNode* head = nullptr;
+        AnimalNode* currentAnimal = nullptr;
+
+        while (getline(myFile, line))
         {
-            fileContents += line + '\n';
+            // Create a new animal node
+            AnimalNode* newNode = new AnimalNode;
+            newNode->animalName = line;
+            newNode->next = nullptr;
+
+            // Add the new node to the linked list
+            if (head == nullptr) {
+                head = newNode;
+                currentAnimal = newNode;
+            }
+            else {
+                currentAnimal->next = newNode;
+                currentAnimal = newNode;
+            }
         }
+
         myFile.close();
+
+        // Print the linked list
+        AnimalNode* current = head;
+        while (current != nullptr)
+        {
+            cout << "Animal: " << current->animalName << endl;
+            current = current->next;
+        }
+
+        // Clean up: Delete the linked list nodes
+        current = head;
+        while (current != nullptr)
+        {
+            AnimalNode* temp = current;
+            current = current->next;
+            delete temp;
+        }
     }
-
-    cout << fileContents;
-    int position = fileContents.find("Hyena");
-    int position2 = fileContents.find("Lion");
-    int position3 = fileContents.find("Bear");
-    int position4 = fileContents.find("Tiger");
-    cout << position << " and " << position2 << " and " << position3 << " and " << position4 << endl;
-
-    int hyenaLineNumber = 0;
-    int lineNumber = 0;
-
+    else
+    {
+        cout << "Failed to open the file." << endl;
+    }
 
     return 0;
 }
